078dbabe11d3b5de93b245d7fd3adf7b9f390dc2
Hisham <93643111+hishm-p@users.noreply.github.com>
2022-03-21 20:17:29 +0530

    Update Pdt.java

--- Exercise 3/Pdt.java
+++ Exercise 3/Pdt.java
@@ -17,7 +17,7 @@ class Pdt
  public static void main(String args[])
  {
   Product p1=new Product("1","Product 1",250);
-  Product p2=new Product("2","Product 2",250);
+  Product p2=new Product("2","Product 2",200);
   Product p3=new Product("3","Product 3",350);
 
   if(p1.price<=p2.price && p1.price<=p3.price)
